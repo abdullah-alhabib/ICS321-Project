c702a48a0271a86513523dd45177db133e639def
abdullah <Abdullah.hb20@gmail.com>
2022-12-15 01:11:02 +0300

    Report interface completed

--- src/main/java/com/example/ics321/ReportController.java
+++ src/main/java/com/example/ics321/ReportController.java
@@ -1,4 +1,41 @@
 package com.example.ics321;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
-public class ReportController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ReportController implements Initializable {
+        @FXML
+        private ComboBox<String> dropDownCategories;
+
+        @FXML
+        private ComboBox<String> dropDownStatus;
+         @FXML
+         private TextField LocationField;
+        @FXML
+        private Button btn_generate;
+
+
+
+    @FXML
+    public void setCategoriesItem (ActionEvent e){
+        dropDownCategories.setItems(FXCollections.observableArrayList("Regular", "Fragile", "Liquid", "Chemical"));
+    }
+    @FXML
+    public void setStatusItem (ActionEvent e){
+        dropDownStatus.setItems(FXCollections.observableArrayList("Delay","Delivered","Damaged","Lost","Ready","In transit", ""));
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        dropDownCategories.setItems(FXCollections.observableArrayList("Regular", "Fragile", "Liquid", "Chemical"));
+        dropDownStatus.setItems(FXCollections.observableArrayList("Delay","Delivered","Damaged","Lost","Ready","In transit", "All"));
+
+    }
 }
--- src/main/java/com/example/ics321/app.java
+++ src/main/java/com/example/ics321/app.java
@@ -11,9 +11,9 @@ import java.io.IOException;
 public class app extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(app.class.getResource("login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(app.class.getResource("Report.fxml"));
         stage.initStyle(StageStyle.UNDECORATED);
-        Scene scene = new Scene(fxmlLoader.load(), 960, 540);
+        Scene scene = new Scene(fxmlLoader.load(), 1024, 720);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
